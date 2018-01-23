58cc9af1f58621fe94bb846104b7cb059a4f580b
Francois Suter <fsuter@cobweb.ch>
2018-01-23 20:42:38 +0100

    Release version 3.2.0

--- Documentation/Introduction/Index.rst
+++ Documentation/Introduction/Index.rst
@@ -44,9 +44,18 @@ Questions and support
 
 If you have any questions about this extension, please ask them in the
 TYPO3 English mailing list, so that others can benefit from the
-answers. Please use the bug tracker on forge.typo3.org to report
+answers. Please use the bug tracker on GitHub to report
 problem or suggest features
 (https://github.com/cobwebch/svconnector/issues).
+
+
+.. _credits:
+
+Credits
+^^^^^^^
+
+The :code:`\Cobweb\Svconnector\Utility\FileUtility` feature was
+sponsored by `h_da - Hochschule Darmstadt, University of Applied Sciences <https://h-da.de/>`_.
 
 
 .. _happy-developer:
--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'createDirs' => '',
   'clearCacheOnLoad' => 1,
   'author_company' => '',
-  'version' => '3.1.0',
+  'version' => '3.2.0',
   'constraints' =>
   array (
     'depends' =>
@@ -36,6 +36,5 @@ $EM_CONF[$_EXTKEY] = array (
       'devlog' => '',
     ),
   ),
-  '_md5_values_when_last_written' => 'a:35:{s:9:"ChangeLog";s:4:"2059";s:11:"LICENSE.txt";s:4:"6404";s:9:"README.md";s:4:"cca8";s:13:"composer.json";s:4:"d76c";s:21:"ext_conf_template.txt";s:4:"24ae";s:12:"ext_icon.png";s:4:"29de";s:14:"ext_tables.php";s:4:"faf6";s:40:"Classes/Controller/TestingController.php";s:4:"4c14";s:49:"Classes/Domain/Repository/ConnectorRepository.php";s:4:"6edf";s:40:"Classes/Exception/ConnectorException.php";s:4:"ef01";s:42:"Classes/Exception/SourceErrorException.php";s:4:"85c6";s:33:"Classes/Service/ConnectorBase.php";s:4:"9241";s:36:"Classes/Utility/ConnectorUtility.php";s:4:"d165";s:43:"Classes/ViewHelpers/Be/ResultViewHelper.php";s:4:"bd60";s:26:"Documentation/Includes.txt";s:4:"c83c";s:23:"Documentation/Index.rst";s:4:"69ae";s:26:"Documentation/Settings.yml";s:4:"45cb";s:25:"Documentation/Targets.rst";s:4:"cc7b";s:37:"Documentation/Configuration/Index.rst";s:4:"10cd";s:34:"Documentation/Developers/Index.rst";s:4:"76de";s:47:"Documentation/Developers/ConnectorApi/Index.rst";s:4:"4183";s:46:"Documentation/Developers/Conversions/Index.rst";s:4:"7814";s:45:"Documentation/Developers/Exceptions/Index.rst";s:4:"af83";s:40:"Documentation/Developers/Hooks/Index.rst";s:4:"c4c8";s:47:"Documentation/Developers/Implementing/Index.rst";s:4:"a97e";s:47:"Documentation/Developers/Initializing/Index.rst";s:4:"f98d";s:44:"Documentation/Developers/Resetting/Index.rst";s:4:"a857";s:41:"Documentation/Developers/Sample/Index.rst";s:4:"7e13";s:35:"Documentation/Images/TestModule.png";s:4:"23e4";s:36:"Documentation/Installation/Index.rst";s:4:"f7e6";s:36:"Documentation/Introduction/Index.rst";s:4:"5237";s:40:"Resources/Private/Language/locallang.xlf";s:4:"9e49";s:48:"Resources/Private/Templates/Testing/Default.html";s:4:"8bd3";s:45:"Resources/Public/Images/ModuleSvconnector.svg";s:4:"a86b";s:44:"Resources/Public/JavaScript/TestingModule.js";s:4:"bccb";}',
 );
 
