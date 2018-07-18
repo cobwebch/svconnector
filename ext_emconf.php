85bdc2153f6cb5ea2d5ae8fd4d5b0195df046a28
Francois Suter <fsuter@cobweb.ch>
2018-07-18 13:07:03 +0200

    [TASK] Release version 3.2.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'createDirs' => '',
   'clearCacheOnLoad' => 1,
   'author_company' => '',
-  'version' => '3.2.3',
+  'version' => '3.2.4',
   'constraints' =>
   array (
     'depends' =>
