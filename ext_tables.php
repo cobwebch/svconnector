2afd8bf2be9b3c2cbb610b406fcd1ad4c4164608
Francois Suter <sutfra@gmail.com>
2021-04-06 12:09:45 +0200

    [TASK] Ensure compatibility with TYPO3 11, drop support for TYPO3 8 and 9

--- ext_tables.php
+++ ext_tables.php
@@ -1,27 +1,20 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
 
-// Add module to the BE
-if (TYPO3_MODE === 'BE') {
-	// Register the backend module
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-			'Cobweb.Svconnector',
-            // Make it a submodule of 'Admin Tools'
-			'tools',
-            // Submodule key
-			'tx_Svconnector',
-            // Position
-			'',
-			[
-				// An array holding the controller-action-combinations that are accessible
-				'Testing' => 'default'
-			],
-			[
-					'access' => 'admin',
-					'icon' => 'EXT:svconnector/Resources/Public/Icons/ModuleSvconnector.svg',
-					'labels' => 'LLL:EXT:svconnector/Resources/Private/Language/locallang.xlf'
-			]
-	);
-}
+// Register the backend module
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'Svconnector',
+    // Make it a submodule of 'Admin Tools'
+    'tools',
+    // Submodule key
+    'tx_Svconnector',
+    // Position
+    '',
+    [
+        \Cobweb\Svconnector\Controller\TestingController::class => 'default'
+    ],
+    [
+        'access' => 'admin',
+        'icon' => 'EXT:svconnector/Resources/Public/Icons/ModuleSvconnector.svg',
+        'labels' => 'LLL:EXT:svconnector/Resources/Private/Language/locallang.xlf'
+    ]
+);
