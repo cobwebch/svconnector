26b599b073f01afbd285912fd08c35b157b706e4
François Suter <fsuter@users.noreply.github.com>
2021-04-06 09:12:57 +0200

    Prepare for TYPO3 11, drop support for TYPO3 8 and 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = [
                 [
                         'depends' =>
                                 [
-                                        'typo3' => '8.7.0-10.4.99',
+                                        'typo3' => '10.4.0-11.99.99',
                                 ],
                         'conflicts' =>
                                 [
