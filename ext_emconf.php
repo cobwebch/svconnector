1bbe3c17a1570a7a97964f8971015d0335c3f93b
Francois Suter <>
2023-05-02 22:04:16 +0200

    [TASK] Verify compatibility with TYPO3 12 LTS, release version 5.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,12 +11,12 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'author_company' => '',
-    'version' => '5.0.0',
+    'version' => '5.0.1',
     'constraints' =>
         [
             'depends' =>
                 [
-                    'typo3' => '11.5.0-12.1.99',
+                    'typo3' => '11.5.0-12.4.99',
                 ],
             'conflicts' =>
                 [
